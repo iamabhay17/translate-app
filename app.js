4f306196d441196600a839336a3d1349e3f7cb88
Abhay kumar bhardwaj <2001abhaybhardwaj@gmail.com>
2021-11-05 20:03:18 +0530

    finished

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ var btnTranslate=document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
 var outputDiv=document.querySelector("#output");
 
+var serverURL="https://api.funtranslations.com/translate/dothraki.json";
+function getTranslation(text)
+{
+    return serverURL + "?" + "text=" + text
+}
+
+function errorHandler(error)
+{
+    console.log("Error occured "+ error);
+    alert("Something is wrong with the server ! please try again after sometime")
+}
+
 
 btnTranslate.addEventListener("click",clickEventHandler);
 
 function clickEventHandler()
 {
- outputDiv.innerText= txtInput.value;
+    var inputText =txtInput.value;
+
+    fetch(getTranslation(inputText))
+     .then(response=>response.json())
+     .then(json=>{
+         var translatedText = json.contents.translated;
+         outputDiv.innerText= translatedText;
+     })
+     .catch(errorHandler)
 };
 
